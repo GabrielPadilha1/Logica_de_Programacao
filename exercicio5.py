f9838cf5df94dc97c2084a4c4291a1bba715e5ae
GabrielPadilha1 <gabrielpadilha1705@gmail.com>
2024-07-10 13:50:36 -0300

    versão 1.0

---
+++ exercicio5.py
@@ -0,0 +1,4 @@
+var_numinteiro = int(input("Digite um número inteiro: "))
+var_numdecimal = float(input('Digite um número decimal usando ".": '))
+var_palavra = str(input("Digite uma palavra: "))
+print(f"Você digitou o inteiro {var_numinteiro}, o decimal {var_numdecimal}, e a palavra {var_palavra}")
